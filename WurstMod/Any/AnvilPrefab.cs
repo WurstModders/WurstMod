7e94a43dfba747fe3be633375186ae1f3c05048a
Nathan Gill <nrgill28@gmail.com>
2020-08-13 03:35:28 -0400

    Anvil Prefabs

---
+++ WurstMod/Any/AnvilPrefab.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace WurstMod.Any
+{
+    public class AnvilPrefab : ComponentProxy
+    {
+        public string Guid;
+        public string Bundle;
+        public string AssetName;
+
+        public override void InitializeComponent()
+        {
+            var asset = new Anvil.AssetID();
+            asset.Guid = Guid;
+            asset.Bundle = Bundle;
+            asset.AssetName = AssetName;
+            Debug.Log("Asset bundle name: " + Bundle);
+
+            var callback = AnvilManager.LoadAsync(asset);
+            callback.CompleteNow();
+
+            var prefab = Instantiate(callback.Result, transform.position, transform.rotation);
+            prefab.SetActive(true);
+        }
+
+    }
+}
