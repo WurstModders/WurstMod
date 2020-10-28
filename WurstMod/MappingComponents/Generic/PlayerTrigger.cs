8f27850d3eede3f7fcb832166ad0ac565f075ffe
Nathan Gill <nrgill28@gmail.com>
2020-10-28 16:07:30 -0400

    Player triggers

---
+++ WurstMod/MappingComponents/Generic/PlayerTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace WurstMod.MappingComponents.Generic
+{
+    public class PlayerTrigger : MonoBehaviour
+    {
+        public UnityEvent Enter;
+        public UnityEvent Exit;
+
+        private void OnTriggerEnter(Collider other) => Enter.Invoke();
+
+        private void OnTriggerExit(Collider other) => Exit.Invoke();
+    }
+}
