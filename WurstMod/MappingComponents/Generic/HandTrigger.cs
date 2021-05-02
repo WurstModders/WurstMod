31deb6681fb7323940f2d8c176d66796e57a7f0c
Nathan Gill <nrgill28@gmail.com>
2021-05-01 21:50:28 -0400

    Hand trigger for stuffs

---
+++ WurstMod/MappingComponents/Generic/HandTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using WurstMod.UnityEditor;
+
+namespace WurstMod.MappingComponents.Generic
+{
+    [RequireComponent(typeof(Collider))]
+    public class HandTrigger : ComponentProxy
+    {
+        public UnityEvent Triggered;
+
+        public void OnTriggerEnter(Collider other)
+        {
+            Triggered.Invoke();
+        }
+
+#if UNITY_EDITOR
+        public override void OnExport(ExportErrors err)
+        {
+            // This needs to be a trigger on the interactable layer
+            GetComponent<Collider>().isTrigger = true;
+            gameObject.layer = LayerMask.NameToLayer("HandTrigger");
+        }
+#endif
+    }
+}
